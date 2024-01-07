3a2e571c62bc49b2ec872320c31175d563541e4c
Šarūnas Griškus <sgriskus2002@gmail.com>
2024-01-07 16:32:12 +0200

    fixed types inferring errors

--- src/modules/generic/generic.route.ts
+++ src/modules/generic/generic.route.ts
@@ -1,15 +1,14 @@
 import { FastifyRequest, FastifyReply, HookHandlerDoneFunction } from "fastify";
+import z from "zod";
 
 export const bodyValidationPreHandler =
-  (schema: Zod.AnyZodObject) =>
   <TRequest extends FastifyRequest, TReply extends FastifyReply>(
-    request: TRequest,
-    reply: TReply,
-    done: HookHandlerDoneFunction,
-  ) => {
-    const result = schema.safeParse((request as FastifyRequest).body);
+    schema: z.AnyZodObject,
+  ) =>
+  (request: TRequest, reply: TReply, done: HookHandlerDoneFunction) => {
+    const result = schema.safeParse(request.body);
     if (result.success) return done();
-    return (reply as FastifyReply).code(400).send({
+    return reply.code(400).send({
       message: "At least one validation error has occurred.",
       issues: result.error.errors.map((x) => x.message),
     });
