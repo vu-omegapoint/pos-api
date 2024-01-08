1df5ab42342e88d7b7e3b22c13e217231c8456a4
Šarūnas Griškus <sgriskus2002@gmail.com>
2024-01-09 00:51:16 +0200

    update order schema referential actions

---
+++ prisma/migrations/20240108225020_order/migration.sql
@@ -0,0 +1,38 @@
+-- CreateTable
+CREATE TABLE "BookedItem" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "quantity" INTEGER NOT NULL,
+    "employeeId" TEXT NOT NULL DEFAULT '',
+    "itemId" TEXT NOT NULL,
+    "orderId" TEXT NOT NULL,
+    CONSTRAINT "BookedItem_employeeId_fkey" FOREIGN KEY ("employeeId") REFERENCES "Employee" ("id") ON DELETE SET DEFAULT ON UPDATE CASCADE,
+    CONSTRAINT "BookedItem_itemId_fkey" FOREIGN KEY ("itemId") REFERENCES "Item" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "BookedItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "BookedService" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "startTime" DATETIME NOT NULL,
+    "employeeId" TEXT NOT NULL DEFAULT '',
+    "serviceId" TEXT NOT NULL,
+    "orderId" TEXT NOT NULL,
+    CONSTRAINT "BookedService_employeeId_fkey" FOREIGN KEY ("employeeId") REFERENCES "Employee" ("id") ON DELETE SET DEFAULT ON UPDATE CASCADE,
+    CONSTRAINT "BookedService_serviceId_fkey" FOREIGN KEY ("serviceId") REFERENCES "Service" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "BookedService_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "customerId" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "notes" TEXT,
+    CONSTRAINT "Order_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "Customer" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "BookedItem_itemId_orderId_employeeId_key" ON "BookedItem"("itemId", "orderId", "employeeId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "BookedService_serviceId_orderId_employeeId_key" ON "BookedService"("serviceId", "orderId", "employeeId");
